b03718fb69a1e0c81c7bf20c2afdf358ceafca2b
Joshua Lin <joshua_lin@dcms.nctu.edu.tw>
2017-04-25 23:38:41 +0800

    init

---
+++ NasaSpaceApp/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\joshua\documents\visual studio 2015\Projects\NasaSpaceApp\NasaSpaceApp\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
